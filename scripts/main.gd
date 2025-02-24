327b662349d04061073a7d80728706814e9d8430
David Chang <davidchang7636@gmail.com>
2025-02-23 22:37:49 -0500

    Add submit button

--- scripts/main.gd
+++ scripts/main.gd
@@ -1,5 +1,11 @@
 extends Node2D
 
+const winning_plants: Dictionary = {
+	"Starbloom": null,
+	"Ashthorn": null,
+	"Walking Toad": null,
+}
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -10,5 +16,13 @@ func _process(delta):
 	pass
 
 
-func _on_plant_menu_submit(plants):
+func _on_submit_button_pressed():
+	var plants = $Cauldron.plants
+	var names = {}
+	for p in plants:
+		names[p.plant_name] = null
+	if names == winning_plants:
+		print("win!")
+	else:
+		print("try again!")
 	pass # Replace with function body.
